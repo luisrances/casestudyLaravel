ca59723e3f9defb5b712a26b4c1d0468988efe88
RaferBritney <raferb03@gmail.com>
2025-05-10 01:18:39 +0800

    authentication #1

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,11 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Account extends Model
+class Account extends Authenticatable
 {
-    use HasFactory;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
 
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
     protected $fillable = [
         'first_name',
         'last_name',
@@ -16,4 +25,27 @@ class Account extends Model
         'password',
         'image',
     ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,51 +1,29 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// admin page
-Route::prefix('admin')->group(function () {
-    Route::view('/', 'admin.dashboard.index');
-    Route::view('/dashboard', 'admin.dashboard.index');
-    Route::view('/accounts', 'admin.accounts.index');
-    Route::view('/products', 'admin.products.index');
-    Route::view('/orders', 'admin.orders.index');
-    Route::view('/carts', 'admin.carts.index');
-    Route::view('/carts', 'admin.wishlists.index');
-    Route::view('/payment_details', 'admin.payment_details.index');
+
+Route::get('/', function () {
+    return view('welcome');
 });
 
-// crud customer
-use App\Http\Controllers\AccountController;
-Route::resource('/admin/accounts', AccountController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// crud product
-use App\Http\Controllers\ProductController;
-Route::resource('/admin/products', ProductController::class);
+// Route::get('/admin/dashboard', function () {
+//     return view('admin.admin');
+// })->middleware(['auth'])->name('admin.dashboard');
 
-// crud order
-use App\Http\Controllers\OrderController;
-Route::resource('/admin/orders', OrderController::class);
+Route::get('/admin', function () {
+    return view('admin.dashboard.index');
+})->middleware(['auth'])->name('admin');
 
-// crud cart
-use App\Http\Controllers\CartController;
-Route::resource('/admin/carts', CartController::class);
-
-// crud wiishlist
-use App\Http\Controllers\WishlistController;
-Route::resource('/admin/wishlists', WishlistController::class);
-
-// crud wiishlist
-use App\Http\Controllers\PaymentDetailsController;
-Route::resource('/admin/payment_details', PaymentDetailsController::class);
-
-
-// db check if active
-use Illuminate\Support\Facades\DB;
-Route::get('/db-check', function () {
-    try {
-        DB::connection()->getPdo();
-        return "Connected to DB: " . DB::connection()->getDatabaseName();
-    } catch (\Exception $e) {
-        return "Not connected: " . $e->getMessage();
-    }
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
